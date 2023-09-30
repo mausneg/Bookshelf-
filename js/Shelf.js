96ca70d10ccd2a91e170fc40f187fc23caecd76b
mausneg <maulanasuryanegara123@gmail.com>
2023-09-30 12:47:08 +0800

    fix input, seperate shelf, add checkbox card

--- js/Shelf.js
+++ js/Shelf.js
@@ -10,11 +10,14 @@ function card(title,author,year,isComplete,genre,sinopsis,imgLink){
                             <img class="card-img" src=${imgLink} alt="">
                         </div>
                         <div class="card-body">
-                            <div class="card-title">${title} (${year})</div>
+                            <div class="card-title">${title} (${parseInt(year)})
+                                <div class="card-status">
+                                    ${isComplete ? '<input class="status-check" type="checkbox" checked>' : '<input class="status-check" type="checkbox">'}
+                                </div>
+                            </div>
                             <div class="card-author">${author}</div>
                             <div class="card-genre">${genre}</div>
                             <div class="card-sinopsis">${sinopsis}</div>
-                            <div class="card-status">${isComplete ? 'Done' : 'Undone'}</div>
                             <div class="card-button">
                                 <button class="button-edit"><i class="fa-solid fa-pencil"></i></button>
                                 <button class="button-delete"><i class="fa-solid fa-trash"></i></button>
@@ -44,7 +47,7 @@ function generateDefault(){
             id: null,
             title: 'The Hobbit',
             author: 'J.R.R Tolkien',
-            year: parseInt(1937),
+            year: 1937,
             isComplete: true,
             genre: 'Fantasy', 
             sinopsis: 'Bilbo Baggins, a hobbit who enjoys a comfortable, unambitious life, is swept away by Gandalf the Wizard and a company of dwarves. He embarks on a journey that takes him to the Lonely Mountain where he must face Smaug, a dragon.',
@@ -54,7 +57,7 @@ function generateDefault(){
             id: null,
             title: 'The Lord of The Rings',
             author: 'J.R.R Tolkien',
-            year: parseInt(1954),
+            year: 1954,
             isComplete: false,
             genre: 'Fantasy',
             sinopsis: 'The Lord of the Rings is an epic high fantasy novel written by English author and scholar J. R. R. Tolkien. The story began as a sequel to Tolkien s 1937 fantasy novel The Hobbit, but eventually developed into a much larger work.',
@@ -64,7 +67,7 @@ function generateDefault(){
             id: null,
             title: 'The Chronicles of Narnia',
             author: 'C.S Lewis',
-            year: parseInt(1950),
+            year: 1950,
             isComplete: false,
             genre: 'Fantasy',
             sinopsis: 'The Chronicles of Narnia is a series of fantasy novels by British author C. S. Lewis. Written by Lewis, illustrated by Pauline Baynes, and originally published in London between 1950 and 1956, The Chronicles of Narnia has been adapted for radio, television, the stage, and film.',
@@ -74,7 +77,7 @@ function generateDefault(){
             id: null,
             title: 'The Hunger Games',
             author: 'Suzanne Collins',
-            year: parseInt(2008),
+            year: 2008,
             isComplete: false,
             genre: 'Science Fiction',
             sinopsis: 'The Hunger Games is a 2008 dystopian novel by the American writer Suzanne Collins. It is written in the voice of 16-year-old Katniss Everdeen, who lives in the future, post-apocalyptic nation of Panem in North America.',
@@ -84,7 +87,7 @@ function generateDefault(){
             id: null,
             title: 'Harry Potter',
             author: 'J.K Rowling',
-            year: parseInt(1997),
+            year: 1997,
             isComplete: true,
             genre: 'Fantasy',
             sinopsis: 'Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry.',
@@ -97,17 +100,20 @@ function generateDefault(){
         book.id = generateId();
         storage.setItem('books', JSON.stringify(books));
       }, delay);
-      delay += 100; // Delay selama 1 detik
+      delay += 100;
     }
     storage.setItem('books',JSON.stringify(books))
 }
 function renderBooks(books){
-    const container = document.getElementsByClassName('main-body')[0]
-    container.innerHTML = '';
-    for (const book of books) { 
+    const done = document.getElementById('done');
+    const undone = document.getElementById('undone');
+    done.innerHTML = '';
+    undone.innerHTML = '';
+    for (const book of books) {
         const cardElement = card(book.title,book.author,book.year,book.isComplete,book.genre,book.sinopsis,book.imgLink)
-        container.appendChild(cardElement)
-    } 
+        if(book.isComplete) done.appendChild(cardElement)
+        else undone.appendChild(cardElement)
+    }
     document.querySelectorAll('.button-delete').forEach(deleteButton => {
         deleteButton.addEventListener('click', function() {
             const card = this.closest('.card');
@@ -134,6 +140,19 @@ function renderBooks(books){
             window.location.href = 'AddBook.html';
         });
     });
+    document.querySelectorAll('.status-check').forEach(statusCheck => {
+        statusCheck.addEventListener('change', function() {
+            const card = this.closest('.card');
+            const cardTitleElement = card.querySelector('.card-title');
+            const cardTitle = cardTitleElement.textContent;
+            const title = cardTitle.split(' (')[0];
+            const books = JSON.parse(storage.getItem('books'));
+            const book = books.find(book => book.title === title);
+            book.isComplete = !book.isComplete;
+            storage.setItem('books', JSON.stringify(books));
+            renderBooks(JSON.parse(storage.getItem('books')));
+        });
+    });
 }
 
 let currentSearchResult = [];
@@ -144,10 +163,15 @@ function filterAndSortBooks() {
 
     let books = (currentSearchResult.length > 0) ? currentSearchResult : JSON.parse(storage.getItem('books'))
 
+    document.querySelector('.book-title-undone').removeAttribute('hidden');
+    document.querySelector('.book-title-done').removeAttribute('hidden');
+    
     if (filterValue === 'done') {
         filteredBooks = books.filter(book => book.isComplete === true);
+        document.querySelector('.book-title-undone').setAttribute('hidden', 'hidden');
     } else if (filterValue === 'undone') {
         filteredBooks = books.filter(book => book.isComplete === false);
+        document.querySelector('.book-title-done').setAttribute('hidden', 'hidden');
     } else filteredBooks = books;
 
     let sortedBooks = filteredBooks;
@@ -193,8 +217,6 @@ document.getElementsByClassName('search')[0].addEventListener('input', function 
 document.getElementsByClassName('button-add')[0].addEventListener('click', function() {
     window.location.href = 'AddBook.html';
 })
-
-
 
 
 
